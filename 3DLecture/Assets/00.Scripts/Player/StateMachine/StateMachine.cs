307be78620354ed2a73adb768f218eb9b0925276
JeonCM <wsd3576@gmail.com>
2025-06-10 10:53:33 +0900

    Update StateMachine.cs

--- 3DLecture/Assets/00.Scripts/Player/StateMachine/StateMachine.cs
+++ 3DLecture/Assets/00.Scripts/Player/StateMachine/StateMachine.cs
@@ -1,4 +1,4 @@
-public interface IState
+public interface IState //각각의 상태가 상속하게 될 인터페이스
 {
     public void Enter();
     public void Exit();
@@ -7,27 +7,33 @@ public interface IState
     public void PhysicsUpdate();
 }
 
-public abstract class StateMachine
+public abstract class StateMachine //각 스탯머신이 상속할 기본형
 {
+    //현재 상태 - 인터페이스를 통해 각 필요한 부분을 제현
     protected IState currentState;
 
+    //스탯 변경
     public void ChangeState(IState newState)
     {
+        //지금 상태의 탈출 로직을 실행 후 새 상태를 적용하고 상태 입장
         currentState?.Exit();
         currentState = newState;
         currentState?.Enter();
     }
 
+    //입력 처리
     public void HandleInput()
     {
         currentState?.HandleInput();
     }
 
+    //상태 업데이트
     public void Update()
     {
         currentState?.Update();
     }
 
+    //물리 업데이트
     public void PhysicsUpdate()
     {
         currentState?.PhysicsUpdate();
